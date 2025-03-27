c856d6b9cbd259701b9e915a6e8f0033562af8f1
BearToRear <darpi87@gmail.com>
2025-03-27 12:31:19 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,25 @@
 const express = require('express');
+const connectDB = require('./config/db');
+const authRoutes = require('./routes/authRoutes');
+const groupRoutes = require('./routes/groupRoutes');
+const postRoutes = require('./routes/postRoutes');
+require('dotenv').config();
+const cors = require('cors');
+
 const app = express();
-const PORT = process.env.PORT || 5000;
 
+// Connect to Database
+connectDB();
+
+// Middleware
 app.use(express.json());
+app.use(cors());
 
-app.get('/', (req, res) => {
-    res.send('Welcome to COIN!');
-});
+// Routes
+app.use('/api/auth', authRoutes);
+app.use('/api/groups', groupRoutes);
+app.use('/api/posts', postRoutes);
 
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
-
+// Start Server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
